ff0633ae5db293a28b4b13d0ec2dc5df77537d82
ansara.hooks <ansara.hooks@gmail.com>
2019-03-07 20:41:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,8 +50,10 @@ end
 
 def runner
   welcome
-  initial_round
-  hit (sum)
-  display_card_total
-  end_game
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
